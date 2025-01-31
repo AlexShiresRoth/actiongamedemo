864c094b65dbafda825798fd200abdd5c7838f24
Alex <alexroth96@gmail.com>
2025-01-31 16:42:42 -0500

    Start animation for characters

---
+++ source/ActionGameDemo/Public/Animations/PlayerAnimInstance_USE.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimInstance_USE.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAMEDEMO_API UPlayerAnimInstance_USE : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
